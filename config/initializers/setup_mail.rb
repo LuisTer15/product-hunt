70986280f6f3f24897ac6e447fb38d96b55c7648
LuisTer15 <LuisTer15@github.com>
2019-12-06 11:06:16 -0500

    Config email delivery setup

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -2,10 +2,10 @@ ActionMailer::Base.smtp_settings = {
   address:              'smtp.sendgrid.net',
   port:                  25,
   domain:               'localhost',
-  user_name:            'apikey',
-  password:             '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',
+  user_name:            ENV['smtp_user'],
+  password:             ENV['smtp_password'],
   authentication:       'plain',
   enable_starttls_auto: true  
 }
 
-ActionMailer::Base.default_url_options[:host] = "localhost:3000"
+ActionMailer::Base.default_url_options[:host] = ENV['hostname']
